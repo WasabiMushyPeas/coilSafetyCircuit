1bfe057fe598b71cd001fc1e55a5765c12da2e64
Jackson <54775398+WasabiMushyPeas@users.noreply.github.com>
2024-07-24 09:26:29 -0600

    Refactor test.html to update moisture pump/coil labels and remove commented code

--- ESP32-src/APwebserver/APwebserver.ino
+++ ESP32-src/APwebserver/APwebserver.ino
@@ -25,6 +25,8 @@ String output6State = "off";
 // Coil Relay
 String output22State = "off";
 String output23State = "off";
+String coilTempShutOff = "on";
+String coilPumpMoistureShutOff = "off";
 
 // RGB LED
 String outputLEDState = "off";
@@ -45,6 +47,17 @@ const int input5Thermistor = 5;
 // Moisture Sensors
 const int input20Moisture = 20;
 const int input21Moisture = 21;
+
+// Values
+float temp1;
+float temp2;
+float temp3;
+float temp4;
+float tempAvg;
+int moisture1;
+int moisture2;
+int moistureTotal;
+int coilShutoffTemp = 60;
 
 void setup()
 {
@@ -111,19 +124,7 @@ void loop()
             client.println();
 
             // turns the GPIOs on and off
-            if (header.indexOf("GET /LED/on") >= 0)
-            {
-              Serial.println("GPIO LED on");
-              outputLEDState = "on";
-              digitalWrite(RGB_BUILTIN, HIGH);
-            }
-            else if (header.indexOf("GET /LED/off") >= 0)
-            {
-              Serial.println("GPIO LED off");
-              outputLEDState = "off";
-              digitalWrite(RGB_BUILTIN, LOW);
-            }
-            else if (header.indexOf("GET /6/on") >= 0)
+            if (header.indexOf("GET /6/on") >= 0)
             {
               Serial.println("GPIO 6 on");
               output6State = "on";
@@ -160,54 +161,21 @@ void loop()
               digitalWrite(output23Coil, LOW);
             }
 
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #555555;}</style></head>");
-
-            // Web Page Heading
-            client.println("<body><h1>ESP32 Coil Control Center</h1>");
-
-            // Display current state, and ON/OFF buttons for GPIO LED
-            client.println("<p>LED - " + outputLEDState + "</p>");
-            // If the output26State is off, it displays the ON button
-            if (outputLEDState == "off")
-            {
-              client.println("<p><a href=\"/LED/on\"><button class=\"button\">ON</button></a></p>");
-            }
-            else
-            {
-              client.println("<p><a href=\"/LED/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-
-            // Display current state, and ON/OFF buttons for GPIO 6
-            client.println("<p>Pump - " + output6State + "</p>");
-            // If the output6State is off, it displays the ON button
-            if (output6State == "off")
-            {
-              client.println("<p><a href=\"/6/on\"><button class=\"button\">ON</button></a></p>");
-            }
-            else
-            {
-              client.println("<p><a href=\"/6/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
+            client.println("<!DOCTYPE html>");
+            client.println("<style> .tooltip { position: relative; display: inline-block; } .tooltip .tooltiptext { visibility: hidden; background-color: #E0E3F4; color: #000; text-align: center; border-radius: 5px; padding: 5px 0; /* Position the tooltip */ position: absolute; z-index: 1; top: -35px; } .tooltip:hover .tooltiptext { visibility: visible; } .tooltip:active .tooltiptext { visibility: visible; } a:hover { scale: 1.1; transition: 0.5s; } </style>");
+            client.println("<html lang=\" en \"> <head> <meta charset=\" UTF - 8 \"> <meta name=\" viewport \" content=\" width = device - width, initial - scale = 1.0 \"> <title>Coil Control Panel</title> </head>");
+            client.println("<body> <div style=\" text - align : center; display : flex; flex - wrap : wrap; justify - content : center; flex-direction : column; align-items : center; \"> <h1>Coil Control Panel</h1> <div style=\" display : flex; justify - content : center; background-color: #EEECF4; width : 90 % ; padding : 20px; border-radius : 5px; margin - top : 20px; margin - bottom : 20px; \">");
 
             // Display current state, and ON/OFF buttons for GPIO 22
             client.println("<p>Coil 1 - " + output22State + "</p>");
             // If the output22State is off, it displays the ON button
             if (output22State == "off")
             {
-              client.println("<p><a href=\"/22/on\"><button class=\"button\">ON</button></a></p>");
+              client.println("<a href=\"/22/on\" style=\"text-decoration: none; color: black; \"><div style=\" margin : 20px; background-color: #FD8A8A; padding : 25px; border-radius : 5px; padding-left : 30px; padding-right : 30px; \"> <h2> Coil 1 </h2> </div> </a>");
             }
             else
             {
-              client.println("<p><a href=\"/22/off\"><button class=\"button button2\">OFF</button></a></p>");
+              client.println("<a href=\"/22/off\" style=\"text-decoration: none; color: black; \"><div style=\" margin : 20px; background-color: #A8D1D1; padding : 25px; border-radius : 5px; padding-left : 30px; padding-right : 30px; \"> <h2> Coil 1 </h2> </div> </a>");
             }
 
             // Display current state, and ON/OFF buttons for GPIO 23
@@ -215,17 +183,52 @@ void loop()
             // If the output23State is off, it displays the ON button
             if (output23State == "off")
             {
-              client.println("<p><a href=\"/23/on\"><button class=\"button\">ON</button></a></p>");
+              client.println("<a href=\"/23/on\" style=\"text-decoration: none; color: black; \"><div style=\" margin : 20px; background-color: #FD8A8A; padding : 25px; border-radius : 5px; padding-left : 30px; padding-right : 30px; \"> <h2> Coil 2 </h2> </div> </a>");
             }
             else
             {
-              client.println("<p><a href=\"/23/off\"><button class=\"button button2\">OFF</button></a></p>");
+              client.println("<a href=\"/23/off\" style=\"text-decoration: none; color: black; \"><div style=\" margin : 20px; background-color: #A8D1D1; padding : 25px; border-radius : 5px; padding-left : 30px; padding-right : 30px; \"> <h2> Coil 2 </h2> </div> </a>");
             }
 
-            client.println("</body></html>");
+            // Display current state, and ON/OFF buttons for GPIO 6
+            client.println("<p>Pump - " + output6State + "</p>");
+            // If the output6State is off, it displays the ON button
+            if (output6State == "off")
+            {
+              client.println("<a href=\"/6/on\" style=\"text-decoration: none; color: black; \"><div style=\" margin : 20px; background-color: #FD8A8A; padding : 25px; border-radius : 5px; padding-left : 30px; padding-right : 30px; \"> <h2> Pump </h2> </div> </a>");
+            }
+            else
+            {
+              client.println("<a href=\"/6/off\" style=\"text-decoration: none; color: black; \"><div style=\" margin : 20px; background-color: #A8D1D1; padding : 25px; border-radius : 5px; padding-left : 30px; padding-right : 30px; \"> <h2> Pump </h2> </div> </a>");
+            }
 
-            // The HTTP response ends with another blank line
+            client.println("</div>");
+
+            client.println("<div style=\" display : flex; justify-content : center; flex - wrap : wrap; background-color: #EEECF4; width : 90 % ; padding : 20px; border-radius : 5px; margin - top : 25px; margin - bottom : 25px; \"> <div class=\" tooltip \" style=\" display : flex; flex-direction : column; align-items : center; margin : 20px; background-color: #F7F5FF; padding : 20px; border-radius : 5px; \"> <h1>Coils</h1> <h2>Temp Avg: " + tempAvg + "°C</h2> <h2>Moisture: " + moistureTotal + "</h2> <span class=\" tooltiptext \"> <div style=\" display : flex; align-items : center; \"> <div style=\" margin : 30px; \"> <h2>Coil 1</h2> <h3>Temp 1: " + temp1 + "°C</h3> <h3>Temp 2: " + temp2 + "°C</h3> <h3>Moisture: " + moisture1 + "</h3> </div> <div style=\" margin : 30px; \"> <h2>Coil 2</h2> <h3>Temp 1: " + temp3 + "°C</h3> <h3>Temp 2: " + temp4 + "°C</h3> <h3>Moisture: " + moisture2 + "</h3> </div> </div> </span> </div> </div>");
+
+            client.println("<div style=\"display: flex; justify-content: center; flex-wrap: wrap; background-color: #EEECF4; width: 90%; padding: 20px; border-radius: 5px; margin-top: 25px; margin-bottom: 25px;\">");
+
+            if (coilTempShutOff == "off")
+            {
+              client.println("<a href=\"/Temp/on\" style=\" text-decoration : none; color: black; \"> <div style=\" margin : 30px; background-color: #FD8A8A; padding : 20px; border-radius : 5px; padding-left : 35px; padding-right : 35px; display : flex; flex-direction : column; align-items : center; \"> <div> <h2>Temp Coil Shut Off</h2> <h3> " + coilShutoffTemp + "°C</h3> </div> <div> <a href=\"/Temp/-\" style=\"text-decoration: none; color: black; \"> <button style=\" height : 40px; width : 45px; font - size : 30px; margin : 15px; border-radius : 5px; \">-</button> </a> <a href=\"/Temp/+\" style=\"text-decoration: none; color: black; \"> <button style=\" height : 40px; width : 45px; font - size : 30px; margin : 15px; border-radius : 5px; \">+</button> </a> </div> </div> </a>");
+            }
+            else
+            {
+              client.println("<a href=\"/Temp/off\" style=\" text-decoration : none; color: black; \"> <div style=\" margin : 30px; background-color: #A8D1D1; padding : 20px; border-radius : 5px; padding-left : 35px; padding-right : 35px; display : flex; flex-direction : column; align-items : center; \"> <div> <h2>Temp Coil Shut Off</h2> <h3> " + coilShutoffTemp + "°C</h3> </div> <div> <a href=\"/Temp/-\" style=\"text-decoration: none; color: black; \"> <button style=\" height : 40px; width : 45px; font - size : 30px; margin : 15px; border-radius : 5px; \">-</button> </a> <a href=\"/Temp/+\" style=\"text-decoration: none; color: black; \"> <button style=\" height : 40px; width : 45px; font - size : 30px; margin : 15px; border-radius : 5px; \">+</button> </a> </div> </div> </a>");
+            }
+
+            if (coilPumpMoistureShutOff == "off")
+            {
+              client.println("<a href=\"/PumpMoisture/on\" style=\"text-decoration: none; color: black; \"> <div style=\" margin : 30px; background-color: #FD8A8A; padding : 75px; border-radius : 5px; padding-left : 25px; padding-right : 25px; display : flex; flex-direction : column; align-items : center; \"> <div> <h2>Moisture Pump/Coil Shut Off</h2> </div> </div> </a>");
+            }
+            else
+            {
+              client.println("<a href=\"/PumpMoisture/off\" style=\"text-decoration: none; color: black; \"> <div style=\" margin : 30px; background-color: #A8D1D1; padding : 75px; border-radius : 5px; padding-left : 25px; padding-right : 25px; display : flex; flex-direction : column; align-items : center; \"> <div> <h2>Moisture Pump/Coil Shut Off</h2> </div> </div> </a>");
+            }
+
+            client.println("</div></div></body></html>");
             client.println();
+
             // Break out of the while loop
             break;
           }
